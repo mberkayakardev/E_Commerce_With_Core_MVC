16212b8c4d39f3e2f49e24112475e0c2e55d4182
Berkay Akar <mberkayakar@outlook.com>
2023-11-30 23:00:48 +0300

    finish second section

---
+++ AkarCommerce.MusicStore/AkarCommerce.MusicStore.MVC/Program.cs
@@ -0,0 +1,34 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+
+#region Services
+builder.Services.AddControllersWithViews(); // MVC import
+#endregion
+
+var app = builder.Build();
+
+#region Middlewares
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+{
+    app.UseStatusCodePagesWithReExecute("/status", "{code?}");
+    app.UseExceptionHandler("/Error");
+    app.UseHsts(); // Https e yönlendirme.
+}
+
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+
+    endpoints.MapControllerRoute(name: "default", pattern: "{controller}/{action}/{id?}", defaults: new { controller = "Home", action = "Index" });
+
+});
+
+#endregion
+
+
+app.Run();
+
+
