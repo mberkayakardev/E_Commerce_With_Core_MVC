789eedc32df4305718ca98154a0da3cf2a3a5ef0
Berkay Akar <mberkayakar@outlook.com>
2023-12-14 23:38:43 +0300

    install boostwatch themes

---
+++ TrendMusic.ECommerce/TrendMusic.ECommerce.MVC/TrendMusic.ECommerce.MVC/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrendMusic.ECommerce.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+              
+        }
+    }
+}
--- TrendMusic.ECommerce/TrendMusic.ECommerce.MVC/TrendMusic.ECommerce.MVC/Program.cs
+++ TrendMusic.ECommerce/TrendMusic.ECommerce.MVC/TrendMusic.ECommerce.MVC/Program.cs
@@ -1,15 +1,55 @@
+﻿using Microsoft.Extensions.FileProviders;
 using TrendMusic.ECommerce.Managers.Concrete.DependencyResolves.MicrosoftIOC;
+using TrendMusic.ECommerce.Managers.Concrete.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
 #region Services
-builder.Services.AddControllersWithViews();
-builder.Services.AddCostumeDependencies(builder.Configuration, builder.Environment);
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation(); // Runtime içinde viewdeki değişiklikleri yansıtmak için ilgili ekleme gerçekleştirildi. 
+//builder.Services.AddCostumeDependencies(builder.Configuration, builder.Environment);
 #endregion
+
 var app = builder.Build();
 
 #region Middlewares
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+else
+{
+    app.UseExceptionHandler("/exception");
+    app.UseStatusCodePagesWithReExecute("/errors", "code?={0}"); // Costume Exception Handler
+    app.UseHsts(); // Http yönlendirmeyi zorunlu kılmak için eklenmiştir. 
+}
+
+/// Https Yönlendirme
+app.UseHttpsRedirection(); // Http Yönlendirme için 
+
+/// Static Files Middleware
+app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions { RequestPath = "/node_modules", FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory() + "node_modules")) });
+
+// Routing
 app.UseRouting();
+
+// CORS
+app.UseCors(x => { x.AllowAnyOrigin(); }); // Ajax için gelen her talep burada cors ayarlaması neticesinde filtrelenecek
+
+// Authentication
+app.UseAuthentication();
+
+// Authorization
+app.UseAuthorization();
+
+// Costume Middlewares 
+app.AddCostumeMiddlewares();
+
+// Endpoint (conversational)
+app.UseEndpoints(e => 
+{
+    e.MapControllerRoute(name: "defaults", pattern: "{Area=Customers}/{Controller=Product}/{Action=Index}/{id?}");
+});
 #endregion
 
 app.Run();
