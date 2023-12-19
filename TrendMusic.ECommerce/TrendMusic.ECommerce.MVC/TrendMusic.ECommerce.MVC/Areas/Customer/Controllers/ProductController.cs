d40f79ec1c179d57bdf2e3052595566f324f035f
Berkay Akar <mberkayakar@outlook.com>
2023-12-19 15:32:22 +0300

    trying commit

--- TrendMusic.ECommerce/TrendMusic.ECommerce.MVC/TrendMusic.ECommerce.MVC/Areas/Customer/Controllers/ProductController.cs
+++ TrendMusic.ECommerce/TrendMusic.ECommerce.MVC/TrendMusic.ECommerce.MVC/Areas/Customer/Controllers/ProductController.cs
@@ -9,5 +9,10 @@ namespace TrendMusic.ECommerce.MVC.Areas.Customer.Controllers
         {
             return View();
         }
+
+        public IActionResult Deneme()
+        {
+            return View();
+        }
     }
 }
